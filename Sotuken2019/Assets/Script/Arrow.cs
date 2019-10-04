2df4e53cf2c11254081ecd16269554d766a3057b
HiroyaMasuko <c18015@std.it-college.ac.jp>
2019-10-04 15:24:13 +0900

    Arrowスクリプトを追加(まだ何も書いてない) OVRのコントローラをシーンに配置

---
+++ Sotuken2019/Assets/Script/Arrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        // 3D同士が接触している間、常に呼び出される処理
+        if (collider.gameObject.tag == "Player")
+        {
+            
+        }
+
+    }
+}
